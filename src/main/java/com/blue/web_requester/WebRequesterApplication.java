3e521c65145ccb4db9f348b386d80f3e228dbc24
black6765 <black6765@gmail.com>
2024-07-28 19:27:04 +0900

    setup: set project

---
+++ src/main/java/com/blue/web_requester/WebRequesterApplication.java
@@ -0,0 +1,13 @@
+package com.blue.web_requester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRequesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRequesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blue/web_requester/WebRequesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue.web_requester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRequesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
