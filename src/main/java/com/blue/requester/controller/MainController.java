c247c621d925d6911ab258bcebd1c889c5b7a36c
black6765 <black6765@gmail.com>
2024-08-03 18:43:26 +0900

    update: URL, Header, Body, Response 레이아웃 추가

--- src/main/java/com/blue/requester/controller/MainController.java
+++ src/main/java/com/blue/requester/controller/MainController.java
@@ -3,14 +3,30 @@ package com.blue.requester.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.*;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/test")
-    public String test(Model model) {
+    @PostMapping("/request")
+    public String postMain(
+            @RequestParam("url") String url,
+            @RequestParam("headersKey") String[] headersKeys,
+            @RequestParam("headersValue") String[] headersValues,
+            @RequestParam("body") String body,
+            Model model) {
+
+        Map<String, String> headers = new HashMap<>();
+
+        // 두 배열을 순회하며 Map에 저장
+        for (int i = 0; i < headersKeys.length; i++) {
+            if (!headersKeys[i].isEmpty() && !headersValues[i].isEmpty()) { // 키와 값이 비어있지 않다면
+                headers.put(headersKeys[i], headersValues[i]);
+            }
+        }
 
         Map<String, String> DATA1 = Map.of("item 1", Base64.getEncoder().encodeToString("item 1".getBytes()), "item 2", Base64.getEncoder().encodeToString("item 2".getBytes()));
         Map<String, String> DATA2 = Map.of("item 3", Base64.getEncoder().encodeToString("item 3".getBytes()), "item 4", Base64.getEncoder().encodeToString("item 4".getBytes()));
@@ -32,12 +48,15 @@ public class MainController {
         collections.put("COLLECTION2", datas2);
 
         model.addAttribute("collections", collections);
+        model.addAttribute("body", body);
+        model.addAttribute("result", "result");
+        System.out.println(body);
 
-        return "test";
+        return "request";
     }
 
-    @GetMapping("/")
-    public String mainPage(Model model) {
+    @GetMapping("/request")
+    public String getMain(Model model) {
 
         Map<String, String> DATA1 = Map.of("item 1", Base64.getEncoder().encodeToString("item 1".getBytes()), "item 2", Base64.getEncoder().encodeToString("item 2".getBytes()));
         Map<String, String> DATA2 = Map.of("item 3", Base64.getEncoder().encodeToString("item 3".getBytes()), "item 4", Base64.getEncoder().encodeToString("item 4".getBytes()));
@@ -60,26 +79,7 @@ public class MainController {
 
         model.addAttribute("collections", collections);
 
-//        List<String> DATA1 = List.of("item 1", "item 2");
-//        List<String> DATA2 = List.of("item 3", "item 4");
-//
-//        Map<String, Object> datas1 = new TreeMap<>();
-//        datas1.put("DATA1", DATA1);
-//        datas1.put("DATA2", DATA2);
-//
-//        List<String> DATA3 = List.of("item 5", "item 6");
-//        List<String> DATA4 = List.of("item 7", "item 8");
-//
-//        Map<String, Object> datas2 = new TreeMap<>();
-//        datas2.put("DATA3", DATA3);
-//        datas2.put("DATA4", DATA4);
-//
-//        Map<String, Map<String, Object>> collections = new HashMap<>();
-//        collections.put("COLLECTION1", datas1);
-//        collections.put("COLLECTION2", datas2);
-//
-//        model.addAttribute("collections", collections);
-        return "index";
+        return "request";
     }
 
 }
